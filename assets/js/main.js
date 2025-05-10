4b40ab13e9bc12dfde060bcebdc788284b3c1db9
danko beribak <beribakdanko@gmail.com>
2025-05-11 01:45:19 +0200

    asd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', () => {
   const navbar = document.querySelector('.navbar');
   if (window.scrollY > 50) {
-    navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'; // Dark background when scrolled
+    navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.7)'; // Dark background when scrolled
   } else {
     navbar.style.backgroundColor = 'transparent'; // Transparent background at the top
   }
