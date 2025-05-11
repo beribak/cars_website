f1c936e833137c2ad990e1d7729e7394c6759225
danko beribak <beribakdanko@gmail.com>
2025-05-11 13:39:49 +0200

    add a homepage banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,19 @@ window.addEventListener('scroll', () => {
     navbar.style.backgroundColor = 'transparent'; // Transparent background at the top
   }
 });
+
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  document.getElementById("navbar").classList.toggle("active");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) { // Adjust the scroll threshold as needed
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+});
