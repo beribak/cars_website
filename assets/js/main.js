a8df1bb716b90122e54356f1314ddbeeb6ad1022
danko beribak <beribakdanko@gmail.com>
2025-05-14 02:33:23 +0200

    fix style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 window.addEventListener('scroll', () => {
-  const navbar = document.querySelector('.navbar');
-  const links = document.querySelectorAll('.navbar a'); // Select all links inside the navbar
+  const navbar = document.querySelector('.navbar-custom');
+  const links = document.querySelectorAll('.navbar-custom a'); // Select all links inside the navbar
   const logo = document.querySelector('.logo'); // Select the logo
 
   if (window.scrollY > 50) {
-    navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.9)'; // White background with 90% transparency
+    navbar.style.backgroundColor = 'rgba(255, 255, 255)'; // White background with 90% transparency
     navbar.style.color = '#000'; // Change navbar text color to black
     links.forEach(link => link.style.color = '#000'); // Change link colors to black
     if (logo) logo.style.color = '#000'; // Change logo color to black
@@ -24,6 +24,28 @@ document.addEventListener("DOMContentLoaded", () => {
       header.classList.add("scrolled");
     } else {
       header.classList.remove("scrolled");
+    }
+  });
+});
+
+// mobile menu toggle 
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+  const socialIcons = document.querySelector(".social-icons");
+  const menuIcon = menuToggle.querySelector("i");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active"); // Toggle visibility of nav links
+    socialIcons.classList.toggle("active"); // Toggle visibility of social icons
+
+    // Toggle between burger menu and "X" icon
+    if (menuIcon.classList.contains("fa-bars")) {
+      menuIcon.classList.remove("fa-bars");
+      menuIcon.classList.add("fa-times");
+    } else {
+      menuIcon.classList.remove("fa-times");
+      menuIcon.classList.add("fa-bars");
     }
   });
 });
